d101ee862733d2dc2b28ab83ccae85f38e7b2197
Patricia Wagner <115067751+P-H-Wagner@users.noreply.github.com>
2024-03-20 18:40:05 +0100

    various bug fixes

--- flatNano/plotNtuple.py
+++ flatNano/plotNtuple.py
@@ -16,6 +16,10 @@ ROOT.gStyle.SetOptStat(0)
 ROOT.gROOT.SetBatch(True)
 
 
+########################################
+# TODO: include bkg !
+
+
 #########################################
 ## INPUT                               ##
 #########################################
@@ -41,23 +45,15 @@ names = [branch.GetName() for branch in chain.GetListOfBranches()]
 #selections
 selBasic        = f"(bs_pt_reco_1 == bs_pt_reco_1) && (dsMu_mass < {bsMass})"  # bs cut and reco are not nan
 
-selDsMu         = "(sig == 0)"                                        # select Ds  Mu 
-selDsTau        = "(sig == 1)"                                        # select Ds  Tau 
-selDsStarMu     = "(sig == 5)"                                        # select Ds* Mu
-selDsStarTau    = "(sig == 6)"                                        # select Ds* Tau
+selDsMu         = selBasic + "&& (sig == 0)"                                              # select Ds  Mu 
+selDsTau        = selBasic + "&& (sig == 1)"                                              # select Ds  Tau 
+selDsStarMu     = selBasic + "&& (sig == 5)"                                              # select Ds* Mu
+selDsStarTau    = selBasic + "&& (sig == 6)"                                              # select Ds* Tau
 
-selMu           = "(sig == 0 || sig == 5)"                                  # select mu  signals
-selTau          = "(sig == 1 || sig == 6)"                                  # select tau signals
-selDs           = "(sig == 0 || sig == 1)"                                  # select Ds signals
-selDsStar       = "(sig == 5 || sig == 6)"                                  # select Ds star signals
-
-#Entries
-nTot      = chain.GetEntries()
-nBasic    = chain.GetEntries(selBasic)
-nMu       = chain.GetEntries(selMu)
-nTau      = chain.GetEntries(selTau)
-nDs       = chain.GetEntries(selDs)
-nDsStar   = chain.GetEntries(selDsStar)
+selMu           = selBasic + "&& (sig == 0 || sig == 5)"                                  # select mu  signals
+selTau          = selBasic + "&& (sig == 1 || sig == 6)"                                  # select tau signals
+selDs           = selBasic + "&& (sig == 0 || sig == 1)"                                  # select Ds signals
+selDsStar       = selBasic + "&& (sig == 5 || sig == 6)"                                  # select Ds star signals
 
 #########################################
 ## COLOR SCHEME                        ##
@@ -67,14 +63,14 @@ def getColorAndLabel(var):
 
   "Colors for simple plot of signgle histograms, default" 
 
-  if   "gen"      in var: color = ROOT.kMagenta; label = "Gen"
-  elif "coll"     in var: color = ROOT.kBlack;   label = "Coll."
-  elif "lhcb_alt" in var: color = ROOT.kBlue;    label = "LHCb xyz"
-  elif "lhcb"     in var: color = ROOT.kRed;     label = "LHCb z"
-  elif "reco_1"   in var: color = ROOT.kGreen;   label = "Reco 1"
-  elif "reco_2"   in var: color = ROOT.kOrange;  label = "Reco 2"
+  if   "gen"      in var: color,label = ROOT.kMagenta, "Gen"
+  elif "coll"     in var: color,label = ROOT.kBlack,   "Coll."
+  elif "lhcb_alt" in var: color,label = ROOT.kBlue,    "LHCb xyz"
+  elif "lhcb"     in var: color,label = ROOT.kRed,     "LHCb z"
+  elif "reco_1"   in var: color,label = ROOT.kGreen,   "Reco 1"
+  elif "reco_2"   in var: color,label = ROOT.kOrange,  "Reco 2"
 
-  else: color = ROOT.kGray + 2; label = "Reco"
+  else:                   color,label = ROOT.kGray+2,  "Reco"
 
   return (color,label)
 
@@ -107,16 +103,48 @@ def getLegend(x0 = 0.5, y0 = 0.75, x1 = 0.9, y1 = 0.9, nColumns = 1):
   legend.SetTextSize(0.03)
   legend.SetBorderSize(0)
   legend.SetFillStyle(0)
-  
+  legend.SetNColumns(nColumns) 
+ 
   return legend
+
+def getYMax(histos, norm = True):
+
+  maxis = []
+  for hist in histos:
+    if norm: hist.Scale(1/hist.Integral())
+    maxis.append(hist.GetMaximum())
+ 
+  return max(maxis)
 
 def getReco1Scale(selection, name):
 
-  ntot   = chain.GetEntries(selBasic)
-  nReco1 = chain.GetEntries(selBasic + f"&& (abs({name}_reco_1 - {name}_gen) < abs({name}_reco_2 - {name}_gen)) ")
+  ntot   = chain.GetEntries(selection)
+  nReco1 = chain.GetEntries(selection + f"&& (abs({name}_reco_1 - {name}_gen) < abs({name}_reco_2 - {name}_gen)) ")
     
   return nReco1 / ntot 
 
+def getWeightedReco(histos, name, selection, norm = True):
+
+  #get reco scale
+  reco1Scale = getReco1Scale(selection,name)
+
+  #clone recos
+  reco1Copy = histos[name + "_reco_1"].Clone()
+  reco2Copy = histos[name + "_reco_2"].Clone()
+
+  #normalize
+  if norm: 
+    reco1Copy.Scale(1/histos[name + "_reco_1"].Integral())
+    reco2Copy.Scale(1/histos[name + "_reco_2"].Integral())
+
+    reco1Copy.GetYaxis().SetTitle("a.u.")
+
+  #scale and add them
+  reco1Copy.Scale(reco1Scale) 
+  reco2Copy.Scale(1 - reco1Scale) 
+  reco1Copy.Add(reco2Copy)
+    
+  return reco1Copy 
 
 
 #########################################
@@ -140,92 +168,120 @@ def createHistos(selection):
       histos[var].SetLineColor(color)
       histos[var].SetLineWidth(2)
 
+   print(f"      Selection: {selection} DONE")
+
   return histos
 
 #########################################
 ## DRAWING FUNCTIONS                   ##
 #########################################
 
-def simpleDraw(histos, subfolder):
+########################################
+# Simple, plots all histo models
+
+def simpleDraw(histos):
 
   #only makes directory if not already existing
-  #os.system(f"mkdir -p simpleDraw/{subfolder}")
+  os.system(f"mkdir -p ./simpleDraw/")
 
-  for key in  histos.keys():
+  for var in  histos.keys():
    
     canvas = ROOT.TCanvas("canvas", "Canvas", 800, 600)
     legend = getLegend()
     canvas.cd()
 
     # get default labels
-    _,label = getColorAndLabel(key)
+    _,label = getColorAndLabel(var)
 
-    histos[key].Draw("HIST")
+    histos[var].Draw("HIST")
 
-    legend.AddEntry(histos[key],label, "l")
+    legend.AddEntry(histos[var].GetValue(),label, "l")
     legend.Draw("SAME")
 
-    canvas.SaveAs(f"./simpleDraw/{key}.png")  
+    canvas.SaveAs(f"./simpleDraw/{var}.png")  
 
-def signalDistinction(histos,name,key):
+########################################
+# Plots signal distinction according to key 
+
+def signalDistinction(histos,var,key,norm = True):
+
+  "Histos is a LIST!"
+
+  #only makes directory if not already existing
+  os.system(f"mkdir -p ./{key}/")
 
   # get new colors and labels for distinction
   colors, labels = getColorAndLabelSignalDistinction(key) 
 
   canvas = ROOT.TCanvas("canvas", "Canvas", 800, 600)
-  legend = getLegend(nColumns = len(histos)%2)
+  legend = getLegend(nColumns = 2)
   canvas.cd()
+
+  # get maximum
+  yMax = getYMax(histos,norm)
 
   for i, hist in enumerate(histos):
     
-    # adapt color
-    hist.SetLineColor(colors[i])
-    hist.Scale(1/hist.Integral())
+    hist.SetLineColor(colors[i]) #adapt color
+    if norm: 
+      hist.Scale(1/hist.Integral())
+      hist.GetYaxis().SetTitle("a.u.")
+    hist.SetMaximum(1.3*yMax)
 
     # draw 
     if i == 0: hist.Draw("HIST")
     else:      hist.Draw("HIST SAME")
 
-    #legend.AddEntry(hist,labels[i],"l")
-    #legend.Draw("SAME")
+    legend.AddEntry(hist.GetValue(),labels[i],"l")
+    legend.Draw("SAME")
+ 
+  
+  canvas.SaveAs(f"./{key}/{key}_{var}.png")  
 
-  canvas.SaveAs(f"./mu_vs_tau/{name}_{key}.png")  
+########################################
+# Plots bs reconstruction method distinction 
 
-def methodDistinction(histos,name):
+def methodDistinction(histos,name, selection,norm = True):
+
+  "Histos is a DICTIONARY!"
+
+  #only makes directory if not already existing
+  os.system(f"mkdir -p ./method_dist/")
 
   canvas = ROOT.TCanvas("canvas", "Canvas", 800, 600)
-  legend = getLegend(nColumns = len(histos)%2)
+  legend = getLegend(nColumns = 3)
   canvas.cd()
+
+  yMax = getYMax(histos.values(),norm)
 
   #recreate the weighted combination
   recoAvailable = name + "_reco_1" in models.keys() #check that recos are available 
     
-  if recoAvailable:
+  if recoAvailable: 
+    weightedReco = getWeightedReco(histos,name,selection,norm)
+    weightedReco.Draw("HIST") 
+    weightedReco.SetMaximum(1.3*yMax)
 
-    reco1Scale = getReco1Scale(name)
+  for i, hist in enumerate(histos.values()):
 
-    reco1Copy = histos[name + "_reco_1"].Clone()
-    reco1Copy.Scale(reco1Scale) 
-    reco2Copy = histos[name + "_reco_2"].Clone()
-    reco2Copy.Scale(1 - reco1Scale) 
-    reco1Copy.Add(reco2Copy)
+    var = list(histos.keys())[i]
 
-    reco1Copy.Draw("HIST") 
-     
+    if norm: 
+      hist.Scale(1/hist.Integral())
+      hist.GetYaxis().SetTitle("a.u.")
 
-  for i, hist in enumerate(histos):
-    
-    # adapt color
-    hist.SetLineColor(colors[i])
+    hist.SetMaximum(1.3*yMax)    
+    _,label = getColorAndLabel(var)
 
     # draw 
     if ((i == 0) and (not recoAvailable)): hist.Draw("HIST")
-    else:                         hist.Draw("HIST SAME")
+    else:                                  hist.Draw("HIST SAME")
 
-    #legend.AddEntry(hist,labels[i],"l")
-    #legend.Draw("SAME")
+    legend.AddEntry(hist.GetValue(),label,"l")
+    legend.Draw("SAME")
 
-  canvas.SaveAs(f"./method_distinction/method_{name}.png")  
+  canvas.SaveAs(f"./method_dist/method_dist_{name}.png")  
+  print(f"DONE")
 
 
 #########################################
@@ -234,41 +290,45 @@ def methodDistinction(histos,name):
 
 
 # Create histos for different selections
+print(f" ===> Start creating histograms")
+
 histosBasic     = createHistos(selBasic)
 histosMu        = createHistos(selMu)
 histosTau       = createHistos(selTau)
 histosDs        = createHistos(selDs)
 histosDsStar    = createHistos(selDsStar)
+print(f" ===> Basic, Mu, Tau, Ds, DsStar DONE")
+
 histosDsMu      = createHistos(selDsMu)
 histosDsTau     = createHistos(selDsTau)
 histosDsStarMu  = createHistos(selDsStarMu)
 histosDsStarTau = createHistos(selDsStarTau)
+print(f" ===> Single Signals DONE")
 
 #simpleDraw(histosTau, "tau_only")
 
 # Plot Mu vs Tau
+"""
 for var in models.keys():
   signalDistinction([histosMu[var],histosTau[var]], var, "mutau")
-"""
 # Plot Ds vs DsStar
 for var in models.keys():
   signalDistinction([histosDs[var],histosDsStar[var]], "dsdsstar")
 
-# Plot all signals
+# Plot all signals (Ds mu, Ds tau, Ds* mu Ds* tau)
 for var in models.keys():
   signalDistinction([histosDsMu[var],histosDsTau[var],histosDsStarMu[var],histosDsStarTau[var]], "allsignals")
-
+"""
 #Method comparison
 
-prefix = ["q2","m2miss","cosMuW","cosPhiDs","cosPlaneBs","bs_pt"]
+#prefix = ["q2","m2_miss","cosMuW","cosPhiDs","cosPlaneBs","bs_pt"]
+prefix = ["q2","m2_miss"]
 
 for name in prefix:
+  print(f" ===> Compare Bs reco methods for {name}")
   # for all prefixes get the variables, f.e. m2_miss_gen, m2_miss_coll , ...
-
-  histos = [histosDsMu[var] if name in var for var in models.keys()]
-  
-  methodDistinction(histos,name)
-"""
+  histos = {var: histosDsMu[var] for var in models.keys() if name in var}
+  methodDistinction(histos,name,selDsMu)
 
 
 
