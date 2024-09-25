3c1f56e63975cded0f7137c56a0c9789554f2789
Patricia Wagner <115067751+P-H-Wagner@users.noreply.github.com>
2024-09-25 15:10:23 +0200

    ff naming cleaned

--- hammer/ff_variations_scalar.py
+++ hammer/ff_variations_scalar.py
@@ -33,23 +33,28 @@ names_bgl_coefficients_hammer = [
 #name the eigenvectors like: "e0, e1, .., e9"
 names_eigenvectors = ['e%d' %i for i in range(len(names_bgl_coefficients_hammer))]
 
-#fill the coefficients according to the paper above
+#fill the coefficients according to the paper above 
+# the referenced paper above only considers N = 2 extension, thus coeff a3,b3 and their uncertanties are missing!
 bgl_coefficients = np.array([
- 0.052255946347001495  , # f0  a0 --> Hammer ? 
--0.16027634967890908   , # f0  a1 --> Hammer ? 
- 0.014141836205563255  , # f0  a2 --> Hammer ? 
- 0.0017893827864468802 , # f+  a0 --> Hammer ? 
--0.004691380424494185  , # f+  a1 --> Hammer ? 
--0.015708534616906505  , # f+  a2 --> Hammer ? 
+ 0.052255946347001495  , # f0  a0 --> Hammer  
+-0.16027634967890908   , # f0  a1 --> Hammer  
+ 0.014141836205563255  , # f0  a2 --> Hammer  
+ 0.0                   , # f0  a3 --> Hammer  
+ 0.0017893827864468802 , # f+  b0 --> Hammer  
+-0.004691380424494185  , # f+  b1 --> Hammer  
+-0.015708534616906505  , # f+  b2 --> Hammer  
+ 0.0                   , # f+  b3 --> Hammer  
 ]).astype(np.float64)
 
 bgl_coefficient_uncertainties = np.array([
-0.0007906527485083701  , # f0  a0 --> Hammer ? 
-0.016991421017241458   , # f0  a1 --> Hammer ? 
-0.03169323419790213    , # f0  a2 --> Hammer ? 
-0.00004543889976730505 , # f+  a0 --> Hammer ? 
-0.0008662317680431299  , # f+  a1 --> Hammer ? 
-0.0047035296806361825  , # f+  a2 --> Hammer ? 
+0.0007906527485083701  , # f0  a0 --> Hammer  
+0.016991421017241458   , # f0  a1 --> Hammer  
+0.03169323419790213    , # f0  a2 --> Hammer  
+0.0                    , # f0  a3 --> Hammer  
+0.00004543889976730505 , # f+  b0 --> Hammer  
+0.0008662317680431299  , # f+  b1 --> Hammer  
+0.0047035296806361825  , # f+  b2 --> Hammer  
+0.0                    , # f+  b3 --> Hammer  
 ]).astype(np.float64)
 
 
@@ -57,12 +62,14 @@ bgl_coefficient_uncertainties = np.array([
 
 import numpy as np
 correlation_complete = np.array([
-    [ 1.00000, -0.26188, -0.07456,  0.43994, -0.00314,  0.13858],
-    [-0.26188,  1.00000, -0.18571,  0.28968,  0.10064, -0.09532],
-    [-0.07456, -0.18571,  1.00000, -0.16234,  0.12522, -0.21066],
-    [ 0.43994,  0.28968, -0.16234,  1.00000, -0.72825, -0.23200],
-    [-0.00314,  0.10064,  0.12522, -0.72825,  1.00000,  0.12053],
-    [ 0.13858, -0.09532, -0.21066, -0.23200,  0.12053,  1.00000]    
+    [ 1.00000, -0.26188, -0.07456, 0.0,   0.43994, -0.00314,  0.13858, 0.0],
+    [-0.26188,  1.00000, -0.18571, 0.0,   0.28968,  0.10064, -0.09532, 0.0],
+    [-0.07456, -0.18571,  1.00000, 0.0,  -0.16234,  0.12522, -0.21066, 0.0],
+    [     0.0,      0.0,      0.0, 0.0,       0.0,      0.0,      0.0, 0.0],    
+    [ 0.43994,  0.28968, -0.16234, 0.0,   1.00000, -0.72825, -0.23200, 0.0],
+    [-0.00314,  0.10064,  0.12522, 0.0,  -0.72825,  1.00000,  0.12053, 0.0],
+    [ 0.13858, -0.09532, -0.21066, 0.0,  -0.23200,  0.12053,  1.00000, 0.0],
+    [     0.0,      0.0,      0.0, 0.0,       0.0,      0.0,      0.0, 0.0]    
 ]).astype(np.float64)
 
 corr  = correlation_complete
