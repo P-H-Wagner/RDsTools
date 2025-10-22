37bc29519c31fd3694de9bb09c2a83159ea9a4fc
Patricia Wagner <115067751+P-H-Wagner@users.noreply.github.com>
2025-10-22 16:30:35 +0200

    push helper file before moving to unconstrained fit

--- help/helper.py
+++ help/helper.py
@@ -57,21 +57,38 @@ sig_cons_hammer_24  = "signal_default_29_04_2025_13_51_27" #all events and all b
 ################ 
 
 ## Flat ntuples
-sig_cons_25    = ["16_03_2025_20_56_34"]
-hb_cons_25     = ["17_03_2025_08_36_11"]
-bs_cons_25     = ["17_03_2025_08_37_23"]
-b0_cons_25     = ["17_03_2025_08_37_03"]
-bplus_cons_25  = ["17_03_2025_08_36_31"]
+#sig_cons_25    = ["16_03_2025_20_56_34"]
+#hb_cons_25     = ["29_08_2025_18_35_52"] #["17_03_2025_08_36_11"]
+#bs_cons_25     = ["17_03_2025_08_37_23"]
+#b0_cons_25     = ["17_03_2025_08_37_03"]
+#bplus_cons_25  = ["17_03_2025_08_36_31"]
 
                   #part 1            #part 2            #part 3            #part 4            #part 5
-data_cons_25     = ["20250227_155416", "20250227_161007", "20250227_161505", "20250227_161842", "20250227_161914"]
+#data_cons_25     = ["20250227_155416", "20250227_161007", "20250227_161505", "20250227_161842", "20250227_161914"]
+
+#UNCONSTRAINED FIT     
+data_cons_25     = ["20250915_094746", "20250915_131727", "20250915_131849", "20250915_131952", "20250915_132020"]
+sig_cons_25      = ["23_09_2025_13_17_17"]
+hb_cons_25       = ["23_09_2025_13_12_55"] #(wout pileup info): with pileup: 30_09_2025_09_15_30
+
+# BDT (not even needed for unconstrained fit)
 
 #bdt models, one separate for each trigger
-bdt_model_25_mu7 = "21_07_2025_16_09_20" # this is in the Analysis Note 
-bdt_model_25_mu9 = "21_07_2025_17_14_06" # trained on mu9
+#bdt_model_25_mu7 = "21_07_2025_16_09_20" # this is in the Analysis Note 
+#bdt_model_25_mu9 = "21_07_2025_17_14_06" # trained on mu9
+
+#this is with both OR sign data trained
+bdt_model_25_mu7="15_09_2025_20_27_30"
+bdt_model_25_mu9="15_09_2025_20_58_00"
+#bdt_model_25_mu7="16_09_2025_16_10_56" 
+#bdt_model_25_mu9="16_09_2025_22_13_41"
 
 #data file evaluated on the different trigger bdts
-bdt_data_25     =  "21_07_2025_17_56_45"
+#bdt_data_25     = "21_07_2025_17_56_45"
+#rhis is with both OR sign data trained
+bdt_data_25     = "16_09_2025_21_41_00"
+
+
 
 #signflip fit for relative normalization used in analysis note: 22_07_2025_07_52_30
 
@@ -88,27 +105,36 @@ bdt_data_25     =  "21_07_2025_17_56_45"
 #hb inclusive cocktail list is taken from this folder: inclusive_HbToDsPhiKKPiMuNu_MINI_25mar21_v1 in the /gen repo
 
 # NN model used in analysis note
-#code_25     = "13Jun2025_18h24m10s"
-#code_25     = "10Jul2025_10h51m07s" # with penalty
-#code_25     = "19Jun2025_15h21m59s"
-#code_25_mu9 = "20Jun2025_11h08m24s" #inclues mu9 in the training
+#nn_25_mu7 = "31Aug2025_20h37m02s"
+#this is with both OR sign data trained
+#nn_25_mu7 = "16Sep2025_08h40m52s"
+#this is with unconstrained data but including phipi mass
+#nn_25_mu7 = "23Sep2025_00h05m05s" 
+#this is with unconstrained data excluding phipi mass from training
+#nn_25_mu7 = "23Sep2025_22h35m11s"
 
-#code_25     = "07_07_2025_08_04_17" #heavily overrained
-#code_25     = "11_07_2025_19_33_27" #kfolding 
-#code_25 = "12Jul2025_18h37m10s" #new, more complex NN
-#code_25 = "14_07_2025_11_26_20"
-#code_25 = "14Jul2025_19h46m38s"
-#code_25 = "14Jul2025_22h23m52s"
-#code_25 = "15Jul2025_10h00m59s"
-#code_25 = "13Jun2025_18h24m10s"
-code_25 = "23Jul2025_16h29m19s"
+#this is with new hammer weights
+nn_25_mu7 = "20Oct2025_14h53m39s" #unconstrained data
+nn_25_mu9 = "31Aug2025_22h05m54s"
 
-sig_cons_pastNN_25     = "sig_"    +code_25
-hb_cons_pastNN_25      = "hb_"     +code_25
-bs_cons_pastNN_25      = "bs_"     +code_25
-b0_cons_pastNN_25      = "b0_"     +code_25
-bplus_cons_pastNN_25   = "bplus_"  +code_25
-data_cons_pastNN_25    = "data_"   +code_25
+
+#nn_25_mu7 = "22Oct2025_06h22m43s" # constrained data
+
+cons_pastNN_25_mu7    = {}
+cons_pastNN_25_mu7["sig"  ] = "sig_"    + nn_25_mu7
+cons_pastNN_25_mu7["hb"   ] = "hb_"     + nn_25_mu7
+cons_pastNN_25_mu7["bs"   ] = "bs_"     + nn_25_mu7
+cons_pastNN_25_mu7["b0"   ] = "b0_"     + nn_25_mu7
+cons_pastNN_25_mu7["bplus"] = "bplus_"  + nn_25_mu7
+cons_pastNN_25_mu7["data" ] = "data_"   + nn_25_mu7
+
+cons_pastNN_25_mu9    = {}
+cons_pastNN_25_mu9["sig"  ] = "sig_"    + nn_25_mu9
+cons_pastNN_25_mu9["hb"   ] = "hb_"     + nn_25_mu9
+cons_pastNN_25_mu9["bs"   ] = "bs_"     + nn_25_mu9
+cons_pastNN_25_mu9["b0"   ] = "b0_"     + nn_25_mu9
+cons_pastNN_25_mu9["bplus"] = "bplus_"  + nn_25_mu9
+cons_pastNN_25_mu9["data" ] = "data_"   + nn_25_mu9
 
 #baseline selection
 base_wout_tv_25 = ' && '.join([ #remove the charge and ds+mu mass cuts!
@@ -125,7 +151,17 @@ f'(mu_pt > 8)',
 ])
 
 #hammered signals
-sig_cons_hammer_25 = "signal_default_10_06_2025_08_05_53"
+#sig_cons_hammer_25 = "signal_default_10_06_2025_08_05_53"
+#hammered signals with new harrison weights
+#sig_cons_hammer_25 = "signal_default_01_10_2025_06_55_14"
+# new hammered signals with unconstrained fit
+#sig_cons_hammer_25 ="signal_default_23_09_2025_17_04_03"
+# new hammered signals with unconstrained fit and new harrison weights
+#sig_cons_hammer_25= "signal_default_30_09_2025_22_38_54"
+#if i remove the factor of 2 everywhere..
+#sig_cons_hammer_25 = "signal_default_17_10_2025_16_16_23" #on unconstraiend signal
+sig_cons_hammer_25 = "signal_default_21_10_2025_21_39_09" #on constrained signal
+
 
 isoflip = ' && '.join([ #remove the charge and ds+mu mass cuts! 
 '(rel_iso_03_pv > 0.3)',
@@ -142,6 +178,8 @@ isoflip = ' && '.join([ #remove the charge and ds+mu mass cuts!
 dsmu_gen       = "15_11_2024_06_57_58"
 dsmu_isgw2_gen = "17_11_2024_17_26_37"
 dsstarmu_gen   = "15_11_2024_09_43_21"
+#dsstarmu_isgw2_gen = "19_11_2024_08_23_27"
+dsstarmu_isgw2_gen = "07_10_2025_08_03_59" #with explicit neutrino tagging
 dstau_gen      = "15_11_2024_13_50_26"
 dsstartau_gen  = "16_11_2024_09_45_34"
 
@@ -182,11 +220,11 @@ circularTestPlots = "22_07_2025_08_25_22"
 # unfiltered gen-level weighted files used to calculate the average weight for BCL/BGL
 dsmu_to_bcl       = "dsmu_BCLVar_04_06_2025_14_40_48"
 dsmu_isgw2_to_bcl = "dsmu_isgw2_BCLVar_26_06_2025_12_00_43"
-dsstarmu_to_bgl   = "dsstarmu_BGLVar_04_06_2025_15_03_24"
+dsstarmu_to_bgl   = "dsstarmu_BGLVar_20_10_2025_09_48_08" # hammer with factor 2: "dsstarmu_BGLVar_30_09_2025_22_37_38" #the old (january 25 harrison weights): "dsstarmu_BGLVar_04_06_2025_15_03_24"
 dstau_to_bcl      = "dstau_BCLVar_04_06_2025_14_58_02"
-dsstartau_to_bgl  = "dsstartau_BGLVar_04_06_2025_15_25_47"
+dsstartau_to_bgl  = "dsstartau_BGLVar_20_10_2025_09_37_44" # hammer with factor 2: "dsstartau_BGLVar_30_09_2025_22_37_46" # the old (january 25 harrison weights): "dsstartau_BGLVar_04_06_2025_15_25_47"
 # yaml file used in the analysis note: /work/pahwagne/RDsTools/hammercpp/development_branch/weights/04_06_2025_16_55_31
-averageWeightsYaml = "04_06_2025_16_55_31"
+averageWeightsYaml = "01_10_2025_09_01_19" # the old (nauary 25 harrison weights): "04_06_2025_16_55_31"
 # plots which show central and variational weight effect used in analysis note: 
 # /work/pahwagne/RDsTools/hammercpp/development_branch/weights/plots/10_06_2025_13_04_57/
 
@@ -233,6 +271,7 @@ if __name__ == "__main__":
     "dsmu_gen"      : dsmu_gen,           
     "dsmu_isgw2_gen": dsmu_isgw2_gen, 
     "dsstarmu_gen"  : dsstarmu_gen,   
+    "dsstarmu_isgw2_gen"  : dsstarmu_isgw2_gen,   
     "dstau_gen"     : dstau_gen,       
     "dsstartau_gen" : dsstartau_gen,  
     "dsmu_isgw2_to_cln": dsmu_isgw2_to_cln,
