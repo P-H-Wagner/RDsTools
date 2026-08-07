de41543a798ed42b22dafbdedf967b699bb92fae
Patricia Wagner <115067751+P-H-Wagner@users.noreply.github.com>
2026-08-07 13:44:12 +0200

    save before adding bs kinematics weights

--- skim/create_skimmer.py
+++ skim/create_skimmer.py
@@ -29,62 +29,51 @@ def filesFromFolder(direc):
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('date_time')
-parser.add_argument('channel')     # sig or hb
-parser.add_argument('selection')   # pick selection
-parser.add_argument('constrained', type = boolean_string)
-parser.add_argument('prod')
+parser.add_argument("--date_time", required = True)
+parser.add_argument("--channel"  , required = True)  
+parser.add_argument("--selection", required = True)  
+parser.add_argument("--hammered" , required = True, type = boolean_string)   
+
 args = parser.parse_args()
 
-#python create_skimmer.py 24_04_2025_14_03_22 data base_wout_tv_25 True 25
+#python create_skimmer.py --date_time=20260227_105644 --channel=data --selection=base_wout_tv_25 --hammered=False
+#python create_skimmer.py --date_time=20_03_2026_09_37_22 --channel=sig --selection=minimal --hammered=True
+
+####################
+
+# datetime logic 
+dt_in = args.date_time
+
+now    = datetime.now()
+dt     = now.strftime("%d_%m_%Y_%H_%M_%S") 
+
+####################
+
+if args.hammered:
+  dt_out = dt #save as a new dir without overwriting the old hammer
+else:
+  dt_out = dt_in 
 
 queue = 'short' 
-time = 60
+time = 20
 nevents = -1
-filesPerJob = 20
+filesPerJob = 10
 
 #naming and files
-
-if args.constrained: 
-
-  if args.prod == "24":
-    #2024 production
-    sig_cons = sig_cons_24
-    if    args.channel == 'sig'    : naming = 'all_signals' 
-    elif  args.channel == 'hb'     : naming = 'hb_inclusive'
-    elif  args.channel == 'b0'     : naming = 'b0'          
-    elif  args.channel == 'bs'     : naming = 'bs'          
-    #elif  args.channel == 'lambdab': naming = 'lambdab'    
-    elif  args.channel == 'bplus'  : naming = 'bplus'       
-    else:                            naming = 'data'        
-
-  else:
-    #2025 production
-    sig_cons = sig_cons_25
-    if    args.channel == 'sig'    : naming = 'all_signals' 
-    elif  args.channel == 'hb'     : naming = 'hb_inclusive'
-    elif  args.channel == 'b0'     : naming = 'b0'          
-    elif  args.channel == 'bs'     : naming = 'bs'          
-    #elif  args.channel == 'lambdab': naming = 'lambdab'    
-    elif  args.channel == 'bplus'  : naming = 'bplus'       
-    else:                            naming = 'data'        
-
-if not args.constrained:
-
-  if    args.channel == 'sig'    : naming = 'all_signals' 
-  elif  args.channel == 'hb'     : naming = 'hb_inclusive'
-  elif  args.channel == 'b0'     : naming = 'b0'          
-  elif  args.channel == 'bs'     : naming = 'bs'          
-  #elif  args.channel == 'lambdab': naming = 'lambdab'    
-  elif  args.channel == 'bplus'  : naming = 'bplus'       
-  else:                            naming = 'data'        
-
-
-if args.channel == 'fakes':
-  naming = 'fakes'; files = fakes;
+if    args.channel == 'sig'    : naming = 'all_signals' 
+elif  args.channel == 'hb'     : naming = 'hb_inclusive'
+elif  args.channel == 'b0'     : naming = 'b0'          
+elif  args.channel == 'bs'     : naming = 'bs'          
+#elif  args.channel == 'lambdab': naming = 'lambdab'    
+elif  args.channel == 'bplus'  : naming = 'bplus'       
+else:                            naming = 'data'        
 
 # get chain to access variable names (need all files bc sometimes they are empty for data)
-directory = f"/pnfs/psi.ch/cms/trivcat/store/user/pahwagne/flatNano/{args.date_time}/*"
+if args.hammered:
+  directory = f"/pnfs/psi.ch/cms/trivcat/store/user/pahwagne/hammer/25/signal_default_{dt_in}/*"
+else:
+  directory = f"/pnfs/psi.ch/cms/trivcat/store/user/pahwagne/flatNano/{dt_in}/*"
+
 chain = ROOT.TChain("tree")
 chain.Add(directory)
 
@@ -92,81 +81,65 @@ chain.Add(directory)
 inputfiles = filesFromFolder(directory[:-1]) # remove * used for the chain
 #inputfiles = inputfiles[:1]
 
-#define weights for math solution (take the most actual signal sample)
-signals = ROOT.TChain("tree")
-
-if args.constrained:       input_sig = sig_cons
-if not args.constrained:   input_sig = sig_unc
-if args.channel == 'fakes': input_sig = sig_cons #fakes so far only produced for constrained
-
-for sig in input_sig:
-  signal_directory = f"/pnfs/psi.ch/cms/trivcat/store/user/pahwagne/flatNano/{sig}/*"
-  signals.Add(signal_directory)
-
-# get all non-nan solutions
-def getWeights(name):
-  #print("checking", name)
-  #ntot   = signals.GetEntries("(gen_sig == 0)")                                                                          #check that reco is not nan!
-  #nReco1 = signals.GetEntries(f"(abs({name}_reco_1 - gen_{name}) < abs({name}_reco_2 - gen_{name})) && (gen_sig == 0) && !({name}_reco_1 != {name}_reco_1)")
-  #w1 = nReco1/ntot
-  #print("w1 is: ", w1)
-
-  w1 = 0.5
-
-  return w1 , 1 - w1
-
-
 #branches and names
 branches = chain.GetListOfBranches()
 names    = [branch.GetName() for branch in branches]
 
 # errors and logs
 
-if not os.path.exists(f"./{args.date_time}_{args.selection}/"):
-  os.makedirs(f"{args.date_time}_{args.selection}/logs")
-  os.makedirs(f"{args.date_time}_{args.selection}/errs")
-
-#write for loop as string to print into the skimmer file
+if not os.path.exists(f"./{dt_in}_{args.selection}/"):
+  os.makedirs(f"{dt_in}_{args.selection}/logs")
+  os.makedirs(f"{dt_in}_{args.selection}/errs")
 
 
-forLoop = "df \\"
-
-############################
-# add reco weighted branch #
-############################
-
-w1, w2 = getWeights("bs_pt")
-
-#loop over branches
-
-for name in names:
-  #check if reco is available
-  if "reco_1" in name:
- 
-    # reco_2 string:
-    reco_2 = name.replace("_reco_1", "_reco_2")
-    reco_w = name.replace("_reco_1", "_reco_weighted")
- 
-    #core = name[:-7] # get var name without "_reco_1"
-    #print("core name is: ", core, "var name is: ", name)
-    #w1, w2 = getWeights(core)
-    #forLoop += f"\n.Define(\"{core}_reco_weighted\",\"{w1}*{core}_reco_1 + {w2}*{core}_reco_2\")\\"  
-    forLoop += f"\n.Define(\"{reco_w}\",\"{w1}*{name} + {w2}*{reco_2}\")\\"  
-
-
-############################
-# add pileup weight branch #
-############################
+###############################
+# Corrections and systematics #
+###############################
 
 if args.channel == "sig":
-  forLoop += f"\n.Define('pu_weight',  'get_pu_weights(gen_sig)')\\"
-if args.channel != "data":
-  forLoop += f"\n.Define('trigger_sf', 'get_scale_factors(mu_pt, dxy_mu_sig_pv)')\\"
 
+  #rename time branch to avoid clashes with cpp time library ...
+   
+  forLoop = f"df = df.Define  ('bs_time'    ,'get_time(gen_pv_x, gen_pv_y, gen_pv_z, gen_sv_x, gen_sv_y, gen_sv_z, gen_bs_boost)') \n"
+  forLoop += f"df \\"
 
-############################
-# take snapshot            #
-############################
+  #todo add up/down for PU
+  forLoop += f"\n.Define  ('w_pu'              ,'get_w_pu              (gen_numPUInts)           ') \\"
+  forLoop += f"\n.Define  ('w_pu_up'           ,'get_w_pu_up           (gen_numPUInts)           ') \\"
+  forLoop += f"\n.Define  ('w_pu_down'         ,'get_w_pu_down         (gen_numPUInts)           ') \\"
+
+  #forLoop += f"\n.Redefine('trigger_sf'       ,'get_trigger_sf    (mu_pt, dxy_mu_sig_pv)   ') \\"
+  forLoop += f"\n.Define  ('trigger_sf'        ,'get_trigger_sf    (mu_pt, dxy_mu_sig_pv)   ') \\"
+  forLoop += f"\n.Define  ('trigger_sf_err_hi' ,'get_trigger_sf_err_hi(mu_pt, dxy_mu_sig_pv)   ') \\"
+  forLoop += f"\n.Define  ('trigger_sf_err_lo' ,'get_trigger_sf_err_lo(mu_pt, dxy_mu_sig_pv)   ') \\"
+  #forLoop += f"\n.Define  ('trigger_sf_err'   ,'get_trigger_sf_err(mu_pt, dxy_mu_sig_pv)   ') \\"
+  forLoop += f"\n.Define  ('w_bs_tau'          ,'get_w_bs_tau      (bs_time)                ') \\"
+  forLoop += f"\n.Define  ('w_bs_tau_up'       ,'get_w_bs_tau_up   (bs_time)                ') \\"
+  forLoop += f"\n.Define  ('w_bs_tau_down'     ,'get_w_bs_tau_down (bs_time)                ') \\"
+
+if args.channel == "hb":
+  forLoop  = f"df.Define  ('trigger_sf'        ,'get_trigger_sf    (mu_pt, dxy_mu_sig_pv)') \\"
+  forLoop += f"\n.Define  ('w_pu'              ,'get_w_pu              (gen_numPUInts)           ') \\"
+  forLoop += f"\n.Define  ('w_pu_up'           ,'get_w_pu_up           (gen_numPUInts)           ') \\"
+  forLoop += f"\n.Define  ('w_pu_down'         ,'get_w_pu_down         (gen_numPUInts)           ') \\"
+  forLoop += f"\n.Define  ('trigger_sf_err_hi' ,'get_trigger_sf_err_hi(mu_pt, dxy_mu_sig_pv)') \\"
+  forLoop += f"\n.Define  ('trigger_sf_err_lo' ,'get_trigger_sf_err_lo(mu_pt, dxy_mu_sig_pv)') \\"
+  #forLoop += f"\n.Define  ('trigger_sf_err'   ,'get_trigger_sf_err(mu_pt, dxy_mu_sig_pv)') \\"
+
+if args.channel == "data":
+  forLoop = "df \\"
+ 
+  if (dt_in == "20260227_105644") or (dt_in == "20260227_104615") or (dt_in == "20260227_102607"):
+    #dirty fix
+    #since the period D was produced on crb earlier this year without the 8p5 and 10p5 parts, lets add a column manually
+    #we set the trigger zero all the time, since it was never on in period D!
+    
+    forLoop += f"\n.Define  ('mu8p5_3p5'  , '0.0') \\"
+    forLoop += f"\n.Define  ('mu10p5_3p5' , '0.0') \\"
+
+###############################
+# take snapshot               #
+###############################
 
 forLoop += f"\n.Filter(selec).Snapshot(\"tree\", destination)"
 print(forLoop)
@@ -180,10 +153,30 @@ for i,j in enumerate(range(0, len(inputfiles), filesPerJob)):
   #template
   temp = open("temp_skimmer.py", "rt")
   #file to write to
-  cfg = open(f"{args.date_time}_{args.selection}/skimmer_{i}.py","wt")
+  cfg = open(f"{dt_in}_{args.selection}/skimmer_{i}.py","wt")
 
-  fout = f"/scratch/pahwagne/skimmed_{args.date_time}_{args.selection}/skimmed_{args.date_time}_{args.selection}_chunk_{i}.root"
-  
+
+  fout    = f"/scratch/pahwagne/skimmed_{dt_out}_{args.selection}/skimmed_{dt_out}_{args.selection}_chunk_{i}.root"
+ 
+  if args.hammered:
+    pnfs_loc = f"root://t3dcachedb03.psi.ch:1094///pnfs/psi.ch/cms/trivcat/store/user/pahwagne/hammer/25/signal_default_{dt_out}/skimmed_{args.selection}_{dt_out}_chunk_{i}.root"
+    #pnfs_loc = f"root://t3dcachedb03.psi.ch:1094///pnfs/psi.ch/cms/trivcat/store/user/pahwagne/flatNano/skimmed/{args.selection}/signal_default_{dt_out}_D1_sf_only/skimmed_{args.selection}_{dt_out}_chunk_{i}.root"
+    #pnfs_loc = f"root://t3dcachedb03.psi.ch:1094///pnfs/psi.ch/cms/trivcat/store/user/pahwagne/flatNano/skimmed/{args.selection}/signal_default_{dt_out}_A1_sf_only/skimmed_{args.selection}_{dt_out}_chunk_{i}.root"
+    #os.system(f"mkdir -p /pnfs/psi.ch/cms/trivcat/store/user/pahwagne/flatNano/skimmed/{args.selection}/signal_default_{dt_out}_D1_sf_only/")
+    #os.system(f"mkdir -p /pnfs/psi.ch/cms/trivcat/store/user/pahwagne/flatNano/skimmed/hammer/{args.selection}/signal_default_{dt_out}/")
+  else: 
+    #pnfs_loc = f"root://t3dcachedb03.psi.ch:1094///pnfs/psi.ch/cms/trivcat/store/user/pahwagne/flatNano/skimmed/{args.selection}/{dt_out}/skimmed_{args.selection}_{dt_out}_chunk_{i}.root"
+    #pnfs_loc = f"root://t3dcachedb03.psi.ch:1094///pnfs/psi.ch/cms/trivcat/store/user/pahwagne/flatNano/skimmed/{args.selection}/{dt_out}_D1_sf_only/skimmed_{args.selection}_{dt_out}_chunk_{i}.root"
+    #pnfs_loc = f"root://t3dcachedb03.psi.ch:1094///pnfs/psi.ch/cms/trivcat/store/user/pahwagne/flatNano/skimmed/{args.selection}/{dt_out}_ricc_D_only/skimmed_{args.selection}_{dt_out}_chunk_{i}.root"
+    pnfs_loc = f"root://t3dcachedb03.psi.ch:1094///pnfs/psi.ch/cms/trivcat/store/user/pahwagne/flatNano/skimmed/{args.selection}/{dt_out}_fullBPark/skimmed_{args.selection}_{dt_out}_chunk_{i}.root"
+    #pnfs_loc = f"root://t3dcachedb03.psi.ch:1094///pnfs/psi.ch/cms/trivcat/store/user/pahwagne/flatNano/skimmed/{args.selection}/{dt_out}_A1_sf_only/skimmed_{args.selection}_{dt_out}_chunk_{i}.root"
+
+    #os.system(f"mkdir -p /pnfs/psi.ch/cms/trivcat/store/user/pahwagne/flatNano/skimmed/{args.selection}/{dt_out}/")
+    #os.system(f"mkdir -p /pnfs/psi.ch/cms/trivcat/store/user/pahwagne/flatNano/skimmed/{args.selection}/{dt_out}_D1_sf_only/")
+    #os.system(f"mkdir -p /pnfs/psi.ch/cms/trivcat/store/user/pahwagne/flatNano/skimmed/{args.selection}/{dt_out}_ricc_D_only/")
+    os.system(f"mkdir -p /pnfs/psi.ch/cms/trivcat/store/user/pahwagne/flatNano/skimmed/{args.selection}/{dt_out}_fullBPark/")
+    #os.system(f"mkdir -p /pnfs/psi.ch/cms/trivcat/store/user/pahwagne/flatNano/skimmed/{args.selection}/{dt_out}_A1_sf_only/")
+
 
   for line in temp:
     if   "HOOK_FILE_IN"  in line: 
@@ -191,10 +184,10 @@ for i,j in enumerate(range(0, len(inputfiles), filesPerJob)):
         cfg.write(line.replace("HOOK_FILE_IN", f))
       continue
 
-    elif "HOOK_DATE_TIME"   in line: line = line.replace("HOOK_DATE_TIME", args.date_time)
-    elif "HOOK_SELECTION"   in line: line = line.replace("HOOK_SELECTION", baselines[args.selection])
+    elif "HOOK_DATE_TIME"   in line: line = line.replace("HOOK_DATE_TIME"   , dt_new)
+    elif "HOOK_SELECTION"   in line: line = line.replace("HOOK_SELECTION"   , baselines[args.selection])
     elif "HOOK_NEW_BRANCH"  in line: line = line.replace('"HOOK_NEW_BRANCH"', forLoop)
-    elif "HOOK_FILE_OUT"    in line: line = line.replace("HOOK_FILE_OUT", fout)
+    elif "HOOK_FILE_OUT"    in line: line = line.replace("HOOK_FILE_OUT"    , fout)
   
     cfg.write(line)
 
@@ -204,15 +197,16 @@ for i,j in enumerate(range(0, len(inputfiles), filesPerJob)):
   to_write = '\n'.join([
          '#!/bin/bash',
          'eval "$(conda shell.bash hook)"',
+         #'conda activate /work/pahwagne/environments/skim_env',
          'conda activate /work/pahwagne/environments/hammer3p8',
-         f'mkdir -p /scratch/pahwagne/skimmed_{args.date_time}_{args.selection}',
-         f'python /work/pahwagne/RDsTools/skim/{args.date_time}_{args.selection}/skimmer_{i}.py',
-         f'xrdcp {fout} root://t3dcachedb03.psi.ch:1094///pnfs/psi.ch/cms/trivcat/store/user/pahwagne/flatNano/skimmed/{args.date_time}/skimmed_{args.selection}_{args.date_time}_chunk_{i}.root',
+         f'mkdir -p /scratch/pahwagne/skimmed_{dt_out}_{args.selection}',
+         f'python /work/pahwagne/RDsTools/skim/{dt_in}_{args.selection}/skimmer_{i}.py',
+         f'xrdcp {fout} {pnfs_loc}',
          f'rm -r {fout}',
          '',
      ])
   
-  with open(f"{args.date_time}_{args.selection}/skimmer_{i}.sh", "wt") as flauncher:
+  with open(f"{dt_in}_{args.selection}/skimmer_{i}.sh", "wt") as flauncher:
     flauncher.write(to_write)
   
   
@@ -221,12 +215,12 @@ for i,j in enumerate(range(0, len(inputfiles), filesPerJob)):
         'sbatch',
         f'-p {queue}',
         '--account=t3',
-        f'-o {args.date_time}_{args.selection}/logs/log_{i}.txt',
-        f'-e {args.date_time}_{args.selection}/errs/err_{i}.txt',
+        f'-o {dt_in}_{args.selection}/logs/log_{i}.txt',
+        f'-e {dt_in}_{args.selection}/errs/err_{i}.txt',
         #'--mem=1500M',
         f'--job-name=SKIM_{args.channel}',
         f'--time={time}',
-        f'{args.date_time}_{args.selection}/skimmer_{i}.sh',
+        f'{dt_in}_{args.selection}/skimmer_{i}.sh',
      ])
   
   print(command_sh_batch)
--- skim/temp_skimmer.py
+++ skim/temp_skimmer.py
@@ -1,3 +1,4 @@
+import json
 import ROOT
 import os
 import sys
@@ -18,41 +19,22 @@ print("====> Create rdf")
 df = ROOT.RDataFrame(chain)
 print("====> rdf DONE")
 
-pu_weights    = np.loadtxt("/work/pahwagne/RDsTools/pileup/pu_weights.txt")
-scale_factors = np.loadtxt("/work/pahwagne/RDsTools/data/trigger_scale_factors/scale_factor_matrix.txt")
-pt_edges  = np.loadtxt("/work/pahwagne/RDsTools/data/trigger_scale_factors/pt_edges.txt")
-dxy_edges = np.loadtxt("/work/pahwagne/RDsTools/data/trigger_scale_factors/dxy_edges.txt")
 
-#convert into string such that it can be handed to the cpp process line
-pu_weights_str    = ", ".join(map(str, pu_weights   )) # of type: '0.01, 0.34, 0.89'
-pt_edges_str  = ", ".join(map(str, pt_edges     )) # of type: '0.01, 0.34, 0.89'
-dxy_edges_str = ", ".join(map(str, dxy_edges    )) # of type: '0.01, 0.34, 0.89'
+################################# 
+# PU correction + sys.          #
+################################# 
 
-#scale_factors is a matrix!
-lenx = scale_factors.shape[0]
-leny = scale_factors.shape[1]
+pu_weights           = np.loadtxt("/work/pahwagne/RDsTools/pileup/pu_weights.txt")
+pu_weights_str       = ", ".join(map(str, pu_weights   )) # of type: '0.01, 0.34, 0.89'
 
-scale_factors_str = "{"
+pu_weights_up        = np.loadtxt("/work/pahwagne/RDsTools/pileup/pu_weights_up.txt")
+pu_weights_up_str    = ", ".join(map(str, pu_weights_up   )) # of type: '0.01, 0.34, 0.89'
 
-for i,row in enumerate(scale_factors):
-  sub = "{"
+pu_weights_down      = np.loadtxt("/work/pahwagne/RDsTools/pileup/pu_weights_down.txt")
+pu_weights_down_str  = ", ".join(map(str, pu_weights_down   )) # of type: '0.01, 0.34, 0.89'
 
-  for val in row:
 
-    sub += str(val)
-    if val != row[-1]: sub += ","
-
-  sub += "}"
-  scale_factors_str  += sub
-
-  if i != (len(scale_factors)-1): scale_factors_str += ","
-
-scale_factors_str += "}"
-print(scale_factors_str)
-print(pt_edges_str)
-print(dxy_edges_str)
-
-ROOT.gInterpreter.Declare(rf'''double get_pu_weights(int npv){{
+ROOT.gInterpreter.Declare(rf'''double get_w_pu(int npv){{
 
     float weights_cpp[] = {{{pu_weights_str}}};
     return weights_cpp[npv];
@@ -60,9 +42,137 @@ ROOT.gInterpreter.Declare(rf'''double get_pu_weights(int npv){{
     '''
     )
 
+ROOT.gInterpreter.Declare(rf'''double get_w_pu_up(int npv){{
+
+    float weights_cpp[] = {{{pu_weights_up_str}}};
+    return weights_cpp[npv];
+    }}
+    '''
+    )
+
+ROOT.gInterpreter.Declare(rf'''double get_w_pu_down(int npv){{
+
+    float weights_cpp[] = {{{pu_weights_down_str}}};
+    return weights_cpp[npv];
+    }}
+    '''
+    )
 
 
-ROOT.gInterpreter.Declare(rf'''double get_scale_factors(float mu_pt, float dxy_mu_sig_pv){{
+################################# 
+# Trigger SF correction + sys.  #
+################################# 
+
+scale_factors        = np.loadtxt("/work/pahwagne/RDsTools/corrections/trigger_sf/ricc/trigger_sf_fullBPark.txt")
+scale_factors_err_hi = np.loadtxt("/work/pahwagne/RDsTools/corrections/trigger_sf/ricc/trigger_sf_err_hi_fullBPark.txt")
+scale_factors_err_lo = np.loadtxt("/work/pahwagne/RDsTools/corrections/trigger_sf/ricc/trigger_sf_err_lo_fullBPark.txt")
+pt_edges             = np.loadtxt("/work/pahwagne/RDsTools/corrections/trigger_sf/ricc/pt_edges_fullBPark.txt")
+dxy_edges            = np.loadtxt("/work/pahwagne/RDsTools/corrections/trigger_sf/ricc/dxy_edges_fullBPark.txt")
+
+#scale_factors        = np.loadtxt("/work/pahwagne/RDsTools/corrections/trigger_sf/ricc/trigger_sf_D_only.txt")
+#scale_factors_err_hi = np.loadtxt("/work/pahwagne/RDsTools/corrections/trigger_sf/ricc/trigger_sf_err_hi_D_only.txt")
+#scale_factors_err_lo = np.loadtxt("/work/pahwagne/RDsTools/corrections/trigger_sf/ricc/trigger_sf_err_lo_D_only.txt")
+#scale_factors_err    = np.loadtxt("/work/pahwagne/RDsTools/corrections/trigger_sf/AM/trigger_sf_err_D_only.txt")
+#pt_edges             = np.loadtxt("/work/pahwagne/RDsTools/corrections/trigger_sf/ricc/pt_edges_D_only.txt")
+#dxy_edges            = np.loadtxt("/work/pahwagne/RDsTools/corrections/trigger_sf/ricc/dxy_edges_D_only.txt")
+
+
+#debug
+#scale_factors     = np.loadtxt("/work/pahwagne/RDsTools/corrections/trigger_sf/trigger_sf_D1.txt")
+#scale_factors_err = np.loadtxt("/work/pahwagne/RDsTools/corrections/trigger_sf/trigger_sf_err_D1.txt")
+#pt_edges          = np.loadtxt("/work/pahwagne/RDsTools/corrections/trigger_sf/pt_edges_D1.txt")
+#dxy_edges         = np.loadtxt("/work/pahwagne/RDsTools/corrections/trigger_sf/dxy_edges_D1.txt")
+
+#scale_factors     = np.loadtxt("/work/pahwagne/RDsTools/corrections/trigger_sf/trigger_sf_A1.txt")
+#scale_factors_err = np.loadtxt("/work/pahwagne/RDsTools/corrections/trigger_sf/trigger_sf_err_A1.txt")
+#pt_edges          = np.loadtxt("/work/pahwagne/RDsTools/corrections/trigger_sf/pt_edges_A1.txt")
+#dxy_edges         = np.loadtxt("/work/pahwagne/RDsTools/corrections/trigger_sf/dxy_edges_A1.txt")
+
+
+#convert into string such that it can be handed to the cpp process line
+pt_edges_str  = ", ".join(map(str, pt_edges     )) # of type: '0.01, 0.34, 0.89'
+dxy_edges_str = ", ".join(map(str, dxy_edges    )) # of type: '0.01, 0.34, 0.89'
+
+#scale_factors is a matrix!
+lenx = scale_factors.shape[0]
+leny = scale_factors.shape[1]
+
+scale_factors_str        = "{"
+scale_factors_err_hi_str = "{"
+scale_factors_err_lo_str = "{"
+#scale_factors_err_str    = "{"
+
+############################################
+for i,row in enumerate(scale_factors):
+  sub = "{"
+
+  for j,val in enumerate(row):
+
+    sub += str(val)
+    if j != len(row)-1: sub += ","
+
+  sub += "}"
+  scale_factors_str  += sub
+
+  if i != (len(scale_factors)-1): scale_factors_str += ","
+
+scale_factors_str += "}"
+
+############################################
+for i,row in enumerate(scale_factors_err_hi):
+  sub = "{"
+
+  for j,val in enumerate(row):
+
+    sub += str(val)
+    if j != len(row)-1: sub += ","
+
+  sub += "}"
+  scale_factors_err_hi_str  += sub
+
+  if i != (len(scale_factors_err_hi)-1): scale_factors_err_hi_str += ","
+
+scale_factors_err_hi_str += "}"
+
+############################################
+for i,row in enumerate(scale_factors_err_lo):
+  sub = "{"
+
+  for j,val in enumerate(row):
+
+    sub += str(val)
+    if j != len(row)-1: sub += ","
+
+  sub += "}"
+  scale_factors_err_lo_str  += sub
+
+  if i != (len(scale_factors_err_lo)-1): scale_factors_err_lo_str += ","
+
+scale_factors_err_lo_str += "}"
+
+############################################
+#for i,row in enumerate(scale_factors_err):
+#  sub = "{"
+#
+#  for j,val in enumerate(row):
+#
+#    sub += str(val)
+#    if j != len(row)-1: sub += ","
+#
+#  sub += "}"
+#  scale_factors_err_str += sub
+#
+#  if i != (len(scale_factors_err)-1): scale_factors_err_str += ","
+#
+#scale_factors_err_str += "}"
+#
+
+
+print(scale_factors_str)
+print(pt_edges_str)
+print(dxy_edges_str)
+
+ROOT.gInterpreter.Declare(rf'''double get_trigger_sf(float mu_pt, float dxy_mu_sig_pv){{
 
     float scale_factors_cpp[{lenx}][{leny}] = {scale_factors_str};
     float pt_edges_cpp [{lenx + 1}] = {{{pt_edges_str}}};
@@ -96,7 +206,166 @@ ROOT.gInterpreter.Declare(rf'''double get_scale_factors(float mu_pt, float dxy_m
     '''
     )
 
+ROOT.gInterpreter.Declare(rf'''double get_trigger_sf_err_hi(float mu_pt, float dxy_mu_sig_pv){{
 
+    float scale_factors_err_hi_cpp[{lenx}][{leny}] = {scale_factors_err_hi_str};
+    float pt_edges_cpp [{lenx + 1}] = {{{pt_edges_str}}};
+    float dxy_edges_cpp[{leny + 1}] = {{{dxy_edges_str}}};
+
+    int index_pt  = 0;
+    int index_dxy = 0;
+
+    for(int i=0; i<{lenx}; ++i){{
+
+    if ((mu_pt > pt_edges_cpp[i]) && (mu_pt < pt_edges_cpp[i+1])){{
+        index_pt = i;
+        break;
+      }} 
+    }}
+
+    for(int i=0; i<{leny}; ++i){{
+
+    float dxy_mu_sig_pv_abs = dxy_mu_sig_pv;
+    if (dxy_mu_sig_pv_abs < 0){{dxy_mu_sig_pv_abs = -1 * dxy_mu_sig_pv_abs ;}}
+
+    if ((dxy_mu_sig_pv_abs > dxy_edges_cpp[i]) && (dxy_mu_sig_pv_abs < dxy_edges_cpp[i+1])){{
+        index_dxy = i;
+        break;
+      }}
+    }}
+
+
+    return scale_factors_err_hi_cpp[index_pt][index_dxy];
+    }}
+    '''
+    )
+
+ROOT.gInterpreter.Declare(rf'''double get_trigger_sf_err_lo(float mu_pt, float dxy_mu_sig_pv){{
+
+    float scale_factors_err_lo_cpp[{lenx}][{leny}] = {scale_factors_err_lo_str};
+    float pt_edges_cpp [{lenx + 1}] = {{{pt_edges_str}}};
+    float dxy_edges_cpp[{leny + 1}] = {{{dxy_edges_str}}};
+
+    int index_pt  = 0;
+    int index_dxy = 0;
+
+    for(int i=0; i<{lenx}; ++i){{
+
+    if ((mu_pt > pt_edges_cpp[i]) && (mu_pt < pt_edges_cpp[i+1])){{
+        index_pt = i;
+        break;
+      }} 
+    }}
+
+    for(int i=0; i<{leny}; ++i){{
+
+    float dxy_mu_sig_pv_abs = dxy_mu_sig_pv;
+    if (dxy_mu_sig_pv_abs < 0){{dxy_mu_sig_pv_abs = -1 * dxy_mu_sig_pv_abs ;}}
+
+    if ((dxy_mu_sig_pv_abs > dxy_edges_cpp[i]) && (dxy_mu_sig_pv_abs < dxy_edges_cpp[i+1])){{
+        index_dxy = i;
+        break;
+      }}
+    }}
+
+
+    return scale_factors_err_lo_cpp[index_pt][index_dxy];
+    }}
+    '''
+    )
+
+#ROOT.gInterpreter.Declare(rf'''double get_trigger_sf_err(float mu_pt, float dxy_mu_sig_pv){{
+#
+#    float scale_factors_err_cpp[{lenx}][{leny}] = {scale_factors_err_str};
+#    float pt_edges_cpp [{lenx + 1}] = {{{pt_edges_str}}};
+#    float dxy_edges_cpp[{leny + 1}] = {{{dxy_edges_str}}};
+#
+#    int index_pt  = 0;
+#    int index_dxy = 0;
+#
+#    for(int i=0; i<{lenx}; ++i){{
+#
+#    if ((mu_pt > pt_edges_cpp[i]) && (mu_pt < pt_edges_cpp[i+1])){{
+#        index_pt = i;
+#        break;
+#      }} 
+#    }}
+#
+#    for(int i=0; i<{leny}; ++i){{
+#
+#    float dxy_mu_sig_pv_abs = dxy_mu_sig_pv;
+#    if (dxy_mu_sig_pv_abs < 0){{dxy_mu_sig_pv_abs = -1 * dxy_mu_sig_pv_abs ;}}
+#
+#    if ((dxy_mu_sig_pv_abs > dxy_edges_cpp[i]) && (dxy_mu_sig_pv_abs < dxy_edges_cpp[i+1])){{
+#        index_dxy = i;
+#        break;
+#      }}
+#    }}
+#
+#
+#    return scale_factors_err_cpp[index_pt][index_dxy];
+#    }}
+#    '''
+#    )
+
+
+
+################################# 
+# Bs lifetime correction + sys. #
+################################# 
+
+# load Bs MC lifetime
+with open("/work/pahwagne/RDsTools/corrections/bs_lifetime/lifetimeMC.json") as f:
+    tau_mc = json.load(f)
+    print(f"====> MC lifetime tau is: {tau_mc}")
+
+#PDG lifetime of Bs
+tau_pdg      = 1.526e-12
+tau_pdg_up   = (1.526 + 0.015) * 1e-12
+tau_pdg_down = (1.526 - 0.015) * 1e-12
+
+ROOT.gInterpreter.Declare(r'''double get_time(float pv_x, float pv_y, float pv_z, float sv_x, float sv_y, float sv_z, float beta){
+
+    //remark sv and pv quantities are in cm
+    float distance = sqrt(pow(sv_x - pv_x,2) + pow(sv_y - pv_y,2) + pow(sv_z - pv_z,2));
+  
+    //convert distance to meters
+    distance *= 0.01; 
+  
+    //remark boost is in atural units, i.e. a number in [0,1] (the beta)
+    float gamma = 1.0 / sqrt(1 - pow(beta,2));
+  
+    // use c in SI units to get a time in seconds
+    float c = 299792458.0;
+    float t = distance / (gamma * beta * c);
+  
+    return t;
+
+    }
+    '''
+    )
+
+ROOT.gInterpreter.Declare(rf'''double get_w_bs_tau(double time){{
+
+    float w = (1 / {tau_pdg}) * exp(-time/{tau_pdg}) / ( (1/{tau_mc}) * exp(-time/{tau_mc}) );
+    return w;
+    }}
+    '''
+    )
+ROOT.gInterpreter.Declare(rf'''double get_w_bs_tau_up(double time){{
+    
+    float w = (1 / {tau_pdg_up}) * exp(-time/{tau_pdg_up}) / ( (1/{tau_mc}) * exp(-time/{tau_mc}));
+    return w;
+    }}
+    '''
+    )
+ROOT.gInterpreter.Declare(rf'''double get_w_bs_tau_down(double time){{
+
+    float w = (1 / {tau_pdg_down}) * exp(-time/{tau_pdg_down}) / ( (1/{tau_mc}) * exp(-time/{tau_mc})); 
+    return w;
+    }}
+    '''
+    )
 
 print("====> Define branch and create snapshot")
 "HOOK_NEW_BRANCH"
